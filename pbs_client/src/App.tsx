3a0fe93794a0f31437d41e78737e06b6b42cd890
nguyenducduy2000 <nguyenducduy2000@gmail.com>
2024-07-09 20:50:50 +0700

    client: change framework

---
+++ pbs_client/src/App.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Dropdown from "react-bootstrap/Dropdown";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img
+                        src={reactLogo}
+                        className="logo react"
+                        alt="React logo"
+                    />
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+
+            <Dropdown as={ButtonGroup}>
+                <Button variant="success">Split Button</Button>
+
+                <Dropdown.Toggle
+                    split
+                    variant="success"
+                    id="dropdown-split-basic"
+                />
+
+                <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">
+                        Another action
+                    </Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">
+                        Something else
+                    </Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+        </>
+    );
+}
+
+export default App;
